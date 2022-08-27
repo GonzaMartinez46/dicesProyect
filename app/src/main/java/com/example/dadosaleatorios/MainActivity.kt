a6bd6171f0a3c18dd9a11767342a9056755832ca
Gonzalo Martinez <gonzamartinez46@gmail.com>
2022-08-26 23:48:13 -0300

    ImageViewDice Finish

--- app/src/main/java/com/example/dadosaleatorios/MainActivity.kt
+++ app/src/main/java/com/example/dadosaleatorios/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.dadosaleatorios
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,31 +15,58 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button= findViewById(R.id.buttonRoll)
-        val caraText: TextView= findViewById(R.id.textCara)
         val contadorText : TextView= findViewById(R.id.textContador)
+        val diceImage: ImageView= findViewById(R.id.imageDiceView)
         val dado1=Dice(6)
         var contador=0
+        var valorRoll:Int
 
+
+        contadorText.text="NUMBER ROLLs: "
         contadorText.textSize=30f
 
 
         rollButton.setOnClickListener{
             contador++
-            caraText.text = dado1.roll().toString()
-            contadorText.text="NUMBER ROLL: ${contador.toString()}"
-            if(dado1.roll()>=4){
-                Toast.makeText(this, "great", Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(this, "retry", Toast.LENGTH_SHORT).show()
-            }
+            valorRoll=dado1.roll()
 
-            when()
+            contadorText.text="NUMBER ROLLs: ${contador.toString()}"
+
+            dado1.cambiarImagen(diceImage, valorRoll)
+
+            mostrarMensaje(valorRoll,6)
+
+
         }
     }
-}
-class Dice(val side:Int){
+fun mostrarMensaje( sideDice:Int, winCondition: Int){
+    when(sideDice){
+        winCondition-> Toast.makeText(this,"GREAAAAT, WIN CHICKEN DINNER", Toast.LENGTH_SHORT).show()
+        1-> Toast.makeText(this,"LOSEE, ROLL DOWN", Toast.LENGTH_SHORT).show()
+        2-> Toast.makeText(this,"LOSEE, ROLL DOWN", Toast.LENGTH_SHORT).show()
+        3-> Toast.makeText(this,"LOSEE, ROLL DOWN", Toast.LENGTH_SHORT).show()
+        4-> Toast.makeText(this,"LOSEE, ROLL DOWN", Toast.LENGTH_SHORT).show()
+        5-> Toast.makeText(this,"LOSEE, ROLL DOWN", Toast.LENGTH_SHORT).show()
 
-    fun roll():Int{
-        return (1..side).random()
+
     }
+}
+}
+class Dice(val sides:Int){
+
+    fun roll():Int{ return(1..sides).random() }
+
+    fun cambiarImagen(imagen: ImageView, sideDice: Int){
+
+        when(sideDice){
+            1->imagen.setImageResource(R.drawable.dice_1)
+            2->imagen.setImageResource(R.drawable.dice_2)
+            3->imagen.setImageResource(R.drawable.dice_3)
+            4->imagen.setImageResource(R.drawable.dice_4)
+            5->imagen.setImageResource(R.drawable.dice_5)
+            6->imagen.setImageResource(R.drawable.dice_6)
+        }
+    }
+
+
 }
